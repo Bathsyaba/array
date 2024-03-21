f0afb89ab5e215db2495cd179f4883395167b4eb
bathba27@gmail.com <>
2024-03-21 11:48:28 +0700

    array1

---
+++ src/modul1/nilaiganjil.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul1;
+
+/**
+ *
+ * @author LENOVO
+ */
+import javax.swing.JOptionPane;
+public class nilaiganjil {
+  public static void main(String[] args) {
+        int data[][] = {{4, 6, 4, 2, 8, 4, 2, 10}, {4, 6, 4, 2, 8, 4, 2, 10}};
+        int totalkolom = 0;
+        for (int a = 0; a < data.length; a++) {
+            for (int b = 0; b < data[0].length; b++) {
+                if (b % 2 != 0) {
+                    totalkolom += data[a][b];
+                }
+
+            }
+            
+        }
+        System.out.print("Total elemen dengan index kolom ganjil :" +totalkolom);
+    }      
+}
